4a70b0eade40994dd03c557a52cb1e760b4aa735
Žiga Vukčevič <ziga@be-codified.com>
2019-01-25 16:15:47 +0100

    Add more samples

---
+++ vue-js/App.vue
@@ -0,0 +1,63 @@
+<!-- ***** Template ***** -->
+<template>
+  <div id="app">
+    <vue-headful
+      title="Vue.js tutorials | Course: list component"
+      description="Some desc"
+    />
+    <h1><span>Vue.js tutorials</span><br />Course: list component</h1>
+
+    <h2>List with items</h2>
+    <List :items="items" :config="{
+      search: true,
+      order: true,
+      itemAdd: true,
+      itemEdit: true,
+      itemRemove: true,
+    }" />
+  </div>
+</template>
+<!-- ***** / Template *** -->
+
+<!-- ***** Script ******* -->
+<script>
+import List from './components/List.vue';
+import items from './data/items';
+
+export default {
+  name: 'app',
+  components: {
+    List,
+  },
+  data() {
+    return {
+      items,
+    };
+  },
+};
+</script>
+<!-- ***** / Script ***** -->
+
+<!-- ***** Style ******** -->
+<style lang="scss">
+// Importing icons
+@import '../node_modules/line-awesome/css/line-awesome.min.css';
+
+// App
+#app {
+  font-family: $font-family;
+  padding: 20px;
+}
+
+// Table
+th {
+  &:nth-child(1),
+  &:nth-child(2) { width: 105px; }
+  &:nth-child(3) { width: 215px; }
+  &:nth-child(4) { width: 120px; }
+}
+
+// Icons
+.fa { font-size: 16px !important; }
+</style>
+<!-- ***** / Style ****** -->
